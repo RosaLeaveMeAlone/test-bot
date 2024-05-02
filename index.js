d258cb7aa44a22e4e479460f1bb38ea7b4544103
Joel Uzcategui <Joeluzcategui102@gmail.com>
2024-05-02 09:44:52 -0400

    First steps

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.API_KEY);
+// console.log('env',process.env);
+
+bot.start((ctx) => {
+    ctx.reply('Hola mundo');
+});
+
+bot.command('asistente', (ctx) => {
+    ctx.reply('Aprendiz de Link');
+});
+
+if(process.env.APP_ENV == 'prod') {
+    bot.launch({
+        webhook: {
+            domain: process.env.APP_URL,
+            port: process.env.APP_PORT,
+        },
+      });
+} else {
+    bot.launch();
+}
