4802a747fec5eae2a1263e2ac4a7fb2a3257b3da
Joel Uzcategui <Joeluzcategui102@gmail.com>
2024-05-02 14:29:45 -0400

    Help message

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const bot = new Telegraf(process.env.API_KEY);
 // console.log('env',process.env);
 
 bot.start((ctx) => {
-    ctx.reply('Hola mundo');
+    ctx.reply('Mensaje de bienvenida');
 });
+
+bot.help((ctx) => {
+    ctx.reply('Mensaje de ayuda');
+});
+
+bot.hears('hola', (ctx) => ctx.reply('Hola que tal'));
 
 bot.command('asistente', (ctx) => {
     ctx.reply('Aprendiz de Link');
